7865d1281ef4e8d10f7b2f49c7416195118d6730
anzii <luoshunan1994@gmail.com>
2021-06-30 17:20:34 +0800

    point_net

---
+++ point_net/src/common/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project: point_net 
+@Author: anzii.Luo
+@Describe: 
+@Date: 2021/6/30
+"""
---
+++ point_net/src/common/input_transform_net.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project: point_net 
+@Author: anzii.Luo
+@Describe: 输入数据转换网络
+@Date: 2021/6/30
+"""
+
+def input_transform_net():
+    pass
