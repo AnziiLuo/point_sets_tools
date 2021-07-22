ca59f13bb6396048920edf0b108557319fa3c546
anzii <luoshunan1994@gmail.com>
2021-07-22 20:25:45 +0800

    point_net

--- point_net/src/models/pointnet_cls.py
+++ point_net/src/models/pointnet_cls.py
@@ -13,7 +13,10 @@ from src.common import input_transform_net
 
 class PointNetModel:
     def __init__(self, point_cloud: tf.Tensor):
-        pass
+        self.point_cloud = point_cloud
 
     def get_model(self):
-        pass
+
+        # 输入点云转换
+        transform = input_transform_net(point_cloud=self.point_cloud, out_dim=3)
+        point_cloud_transform = tf.matmul(self.point_cloud, transform)
